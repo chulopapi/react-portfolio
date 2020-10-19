f2ba2fc2390e50e3db15c0022e426c02276acced
chulopapi <marlon.guandique@gmail.com>
2020-10-18 21:52:03 -0700

    App.js setting up the app function app/imports

--- src/App.js
+++ src/App.js
@@ -23,4 +23,35 @@ function App() {
   );
 }
 
+
+// import React, { useState } from 'react';
+// import Header from './components/Header';
+// import About from './components/About';
+// import Contact from './components/Contact';
+// import Portfolio from './components/Portfolio';
+// import Resume from './components/Resume';
+// import Footer from './components/Footer';
+// import './App.css';
+
+// function App() {
+//   const [pages] = useState(["about me", "portfolio", "contact", "resume"]);
+//   const [pageSelected, setPageSelected] = useState(pages[0]);
+
+//   return (
+//     <div id="html">
+//       <Header pageSelected={pageSelected}
+//         setPageSelected={setPageSelected} />
+//       <main className="light-blue darken-4 white-text">
+//         {(pageSelected === "about me") && <About />}
+//         {(pageSelected === "portfolio") && <Portfolio />}
+//         {(pageSelected === "contact") && <Contact />}
+//         {(pageSelected === "resume") && <Resume />}
+//       </main>
+//       <Footer></Footer>
+//     </div>
+//   );
+// }
+
+
+
 export default App;
