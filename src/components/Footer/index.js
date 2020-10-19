20707d2c4692686b28eaacda7c55aec486b040f3
chulopapi <marlon.guandique@gmail.com>
2020-10-18 22:01:17 -0700

    adding components for header, footer and navigation

---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <section id="footer">
+      <nav>
+        <ul id="external links">
+          <li>
+            <a href="https://github.com/chulopapi" target="_blank"><i className="fab fa-github"></i></a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/marlon.guandique/"
+              target="blank"><i className="fab fa-linkedin"></i></a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+//Written by Marlon Guandique
+import React from "react";
+import Nav from '../../components/Navigation';
+import '../../App.css';
+
+function Header(props) {
+
+  const {
+    pageSelected,
+    setPageSelected
+  } = props;
+
+  return (
+    <header className="flex-row black">
+      <h2 id="header">
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="name"></span> Marlon Guandique
+      </a>
+      </h2>
+      <Nav pageSelected={pageSelected}
+        setPageSelected={setPageSelected} />
+    </header>
+  );
+}
+
+export default Header;
