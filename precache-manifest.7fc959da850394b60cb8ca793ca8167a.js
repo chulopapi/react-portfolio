a223796800013e8e43b2e6ef8159025e1636565e
chulopapi <marlon.guandique@gmail.com>
2020-11-02 16:57:25 -0800

    Updates

---
+++ precache-manifest.7fc959da850394b60cb8ca793ca8167a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba96f4dcda686ec63a7f2896f8d19dd5",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "367b699ecab10ceaa9c3",
+    "url": "/react-portfolio/static/css/main.471ed2e6.chunk.css"
+  },
+  {
+    "revision": "aba9ea93b277a2031fac",
+    "url": "/react-portfolio/static/js/2.b672b5c2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.b672b5c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367b699ecab10ceaa9c3",
+    "url": "/react-portfolio/static/js/main.ce2ebfbc.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "f9c069734edc25f57cb5eecc31cc7733",
+    "url": "/react-portfolio/static/media/SafeTravels.f9c06973.png"
+  },
+  {
+    "revision": "bfbad44a6c8bb1ac8db11136b6801603",
+    "url": "/react-portfolio/static/media/colossal-closer.bfbad44a.png"
+  },
+  {
+    "revision": "8767a4f7736f5af2bcde866b90bf191f",
+    "url": "/react-portfolio/static/media/mypicture.8767a4f7.jpg"
+  },
+  {
+    "revision": "a1a153122a7da26a8bf4d3413468ab88",
+    "url": "/react-portfolio/static/media/safr-2.a1a15312.png"
+  }
+]);
